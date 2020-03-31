13de2a178c17d96377b742402561a008b6a8c947
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-03-31 21:56:32 +0300

    added limit to number of languages requested (GET)

--- routes/languages.js
+++ routes/languages.js
@@ -4,11 +4,19 @@ const mysql = require('../connection');
 
 router.get('/', (req, res) => {
     let query = 'SELECT * FROM languages';
+    let query_two = 'SELECT * FROM languages LIMIT ?';
     
-    mysql.query(query, (error, values) => {
-        if(error) throw error;
-        res.status(200).json(values);
-    })
+    if(!req.query.limit) {
+        mysql.query(query, (error, values) => {
+            if(error) throw error;
+            res.status(200).json(values);
+        })
+    } else {
+        mysql.query(query_two, [parseInt(req.query.limit)], (error, values) => {
+            if(error) throw error;
+            res.status(200).json(values);
+        })
+    }
 });
 
 
