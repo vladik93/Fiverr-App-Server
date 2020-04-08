d806fcfcb3710fb337b8302c8964aeac01e3ca43
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-04-08 12:09:49 +0300

    added route for translators to select only FROM a specific row and the next 9 rows afterwards

--- routes/translators.js
+++ routes/translators.js
@@ -4,11 +4,19 @@ const mysql = require('../connection');
 
 router.get('/', (req, res) => {
     let query = 'SELECT * FROM translators WHERE lang_to = ? LIMIT  ?'; // LIMIT only accepts INTEGERS!
+    let query2 = 'SELECT * FROM translators WHERE lang_to = ? LIMIT ? OFFSET ?';
 
-    mysql.query(query, [req.query.lang_to, parseInt(req.query.limit)], (error, value) => {
-        if(error) throw error;
-        res.status(200).json(value)
-    })
+    if(req.query.offset) {
+        mysql.query(query2, [req.query.lang_to, parseInt(req.query.limit), parseInt(req.query.offset)], (error, value) => {
+            if(error) throw error;
+            res.status(200).json(value);
+        })
+    } else {
+        mysql.query(query, [req.query.lang_to, parseInt(req.query.limit)], (error, value) => {
+            if(error) throw error;
+            res.status(200).json(value);
+        });
+    }
 })
 
 
