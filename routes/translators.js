2d7d96260cb6bdaabc6b1a139f7e5ca715d3d2be
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-04-10 01:47:46 +0300

    made a translator route for the typeahead searchbar feature

--- routes/translators.js
+++ routes/translators.js
@@ -5,21 +5,25 @@ const mysql = require('../connection');
 router.get('/', (req, res) => {
     let query = 'SELECT * FROM translators WHERE lang_to = ? LIMIT  ?'; // LIMIT only accepts INTEGERS!
     let query2 = "SELECT tr.id, tr.name, tr.description, tr.image, tr.lang_from, IFNULL(langfrom.image, 'http://localhost/translation_app/not-specified.png') AS image_from, tr.lang_to, langto.image AS image_to, tr.price FROM translators AS tr INNER JOIN languages AS langto ON tr.lang_to = langto.id LEFT JOIN languages AS langfrom ON tr.lang_from = langfrom.id WHERE lang_to = ? LIMIT ? OFFSET ?";
+    let query3 = "SELECT tr.id, tr.name, tr.lang_from, langfrom.code AS from_code, tr.lang_to, langto.code AS to_code FROM translators AS tr INNER JOIN languages AS langto ON tr.lang_to = langto.id LEFT JOIN languages AS langfrom ON tr.lang_from = langfrom.id"
 
     if(req.query.offset) {
         mysql.query(query2, [req.query.lang_to, parseInt(req.query.limit), parseInt(req.query.offset)], (error, value) => {
             if(error) throw error;
             res.status(200).json(value);
         })
-    } else {
+    } else if(req.query.lang_to) {
         mysql.query(query, [req.query.lang_to, parseInt(req.query.limit)], (error, value) => {
             if(error) throw error;
             res.status(200).json(value);
         });
+    } else if(req.query.typeahead === 'true') {
+        mysql.query(query3, (error, values) => {
+            if(error) throw error;
+            res.status(200).json(values);
+        })
     }
 });
-
-
 
 
 
