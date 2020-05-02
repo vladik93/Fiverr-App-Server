8b1ced4d0ef9b5f0ff41505bcbcc45d1eb6ec0b7
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-05-02 11:07:18 +0300

    added working nodemailer (needs to be refactored)

--- routes/emails.js
+++ routes/emails.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../connection');
 const { checkToken } = require('../middleware/verifyToken');
+const transporter = require('../sender');
 
 router.post('/', (req, res) => {
     let query = 'INSERT INTO subscriptions (`email`) VALUES (?)';
@@ -17,8 +18,37 @@ router.post('/:id', checkToken, (req, res) => {
     let query = 'SELECT u.id AS userId, u.username AS userName, u.email AS userEmail, t.id AS transId, t.name AS transName, t.email AS transEmail, t.phone AS transPhone, lang_from.name AS fromLang, lang_to.name AS toLang FROM users AS u JOIN translators AS t ON t.id = ? LEFT JOIN languages AS lang_from ON t.lang_from = lang_from.id LEFT JOIN languages AS lang_to ON t.lang_to = lang_to.id WHERE u.id = ?';
 
     mysql.query(query, [req.params.id, req.user.id], (error, result) => {
-        if(error) throw error;
-        res.status(200).json(result);
+        if(error) {
+            throw error
+        } else {
+            const mailOptions = {
+                from: '"DevTest" <vladik.semyonov.dev@gmail.com>',
+                to: 'vladik.semyonov@gmail.com',
+                subject: 'Translation Request',
+                html: 
+                `
+                <h4>Request</h4>
+                <h4>User</h4>
+                <ul>
+                    <li>User: ${result[0].userName}</li>
+                    <li>Email: ${result[0].userEmail}</li>
+                </ul>
+                <h4>Translator</h4>
+                <ul>
+                    <li>Translator: ${result[0].transName}</li>
+                    <li>Email: ${result[0].transEmail ? result[0].transEmail : 'Not Specified'}</li>
+                    <li>Phone: ${result[0].transPhone ? result[0].transPhone : 'Not Specified'}</li>
+                    <li>From: ${result[0].fromLang ? result[0].fomLang : 'Not Specified'}</li>
+                    <li>To: ${result[0].toLang ? result[0].toLang : 'Not Specified'}</li>
+                </ul>`
+            }
+            transporter.sendMail(mailOptions, (error, info) => {
+                if(error) throw error;
+                res.send(info);
+                console.log(info);
+            })
+        };
+        
     })
 
 });
--- sender.js
+++ sender.js
@@ -1,11 +1,20 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: process.env.MAIL_SERVICE,
+    host: process.env.MAIL_HOST,
+    port: parseInt(process.env.MAIL_PORT),
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
     }
 });
+
+transporter.verify((error, success) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('Server is ready to take our messages');
+    }
+})
 
 module.exports = transporter;
