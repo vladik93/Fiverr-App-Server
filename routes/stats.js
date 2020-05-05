cbc77fba06f2edf206209d1a82738e9097ac3045
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-05-06 00:23:59 +0300

    made the stats post request (create stats table) part of the registration route (changes can only be made with logged user)

--- routes/stats.js
+++ routes/stats.js
@@ -2,14 +2,6 @@ const router = require('express').Router();
 const { checkToken } = require('../middleware/verifyToken');
 const mysql = require('../connection');
 
-router.post('/',checkToken, (req, res) => {
-    let query = 'INSERT INTO `stats` (`user_id`) VALUES (?)';
-    mysql.query(query, [req.user.id], (error, result) => {
-        if(error) throw error;
-        res.status(200).json(result);
-    })
-})
-
 router.put('/', checkToken, (req, res) => {
     if(req.query.prop === 'visited') {
         let query = 'UPDATE `stats` SET `times_visited` = `times_visited` + 1 WHERE `user_id` = ?';
